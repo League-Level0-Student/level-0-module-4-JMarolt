e8e870c85b63a17b96e36bf72d9a6228baac2d57
League <League@administorsimac.attlocal.net>
2018-03-18 15:58:13 -0700

    PiAloud(done) and sleepyhead(almost done)

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -12,14 +12,16 @@ public class PiAloud {
 	// 1. Make a main method and make sure your program can run
 	public static void main(String[] args) {
 
-	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-String a = 
+	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value 
+	String pi = ("3.1415926535897932384626433832795028841971693993");
 
-JOptionPane.showInputDialog("How many digits of pi should we show you?");
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
-	// 4. Print ALL the digits of of Pi (hint: use a loop)
 
+	// 4. Print ALL the digits of of Pi (hint: use a loop)
+for(int i = 0; i < 47; i++) {
+	System.out.println(pi.charAt(i));
+}
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -9,20 +9,31 @@ public class SleepyHead {
 
     public static void main(String[] args) {
         
-        boolean isWeekday, isVacation;
+        int weekday, vacation;
         
         /*
          * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
+         * 
          */
-    
-
+        {   weekday = JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+     vacation = JOptionPane.showConfirmDialog(null, "is it a vacation?", "", JOptionPane.YES_NO_OPTION);
+         
+System.out.println(weekday);
+if(weekday == 1) {
+	JOptionPane.showMessageDialog(null, "Sleep in!");
+}
+if(weekday == 0) {
+	JOptionPane.showMessageDialog(null, "Get up lazybones!");
+}
+System.out.println(vacation);
 
         /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
          */
+        }
+        vacation = JOptionPane.showConfirmDialog(null, "is it a vacation?", "", JOptionPane.YES_NO_OPTION);
+        System.out.println(vacation);
     }
 }
